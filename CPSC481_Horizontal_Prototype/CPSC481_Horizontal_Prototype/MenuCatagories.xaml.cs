991119324bf5ce280f49d3435bc93bcf0d494098
Bernie Mayer <bemayer@ucalgary.ca>
2016-03-07 17:38:48 -0700

    Added some more interfaces

---
+++ CPSC481_Horizontal_Prototype/CPSC481_Horizontal_Prototype/MenuCatagories.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CPSC481_Horizontal_Prototype
+{
+    /// <summary>
+    /// Interaction logic for MenuCatagories.xaml
+    /// </summary>
+    public partial class MenuCatagories : Window
+    {
+        public MenuCatagories()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
